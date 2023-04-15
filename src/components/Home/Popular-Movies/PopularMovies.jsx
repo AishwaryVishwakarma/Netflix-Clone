8d29a825b71b218e7b16475d4284fc6fadfa6e9c
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2023-04-15 15:41:50 +0530

    movies API req

--- src/components/Home/Popular-Movies/PopularMovies.jsx
+++ src/components/Home/Popular-Movies/PopularMovies.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React from 'react'
+import axios from 'axios'
 
+const API_KEY = '9c3fd4dd152d57af68bd8d3ebd55fce0'
 
-const PopularMovies
-=()=>{
-    return (<div>hello world</div>)
+const PopularMovies = () => {
+  const [movies, setMovies] = React.useState([])
 
+  React.useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`
+      )
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+  }, [])
 
+  console.log(movies)
 
-};
+  return <div>hello world</div>
+}
 
 export default PopularMovies
-;
