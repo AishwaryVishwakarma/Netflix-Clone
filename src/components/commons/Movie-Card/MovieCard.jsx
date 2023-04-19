e7fdc2129cc53b25d8b4b5a2104692279190fdb6
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2023-04-19 15:50:16 +0530

    minor css changes

--- src/components/commons/Movie-Card/MovieCard.jsx
+++ src/components/commons/Movie-Card/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
-const MovieCard = ({poster_path}) => {
+const MovieCard = ({ poster_path }) => {
   return (
     <div>
-    <img src={`https://image.tmdb.org/t/p/original${poster_path}`}/></div>
+      <img src={`https://image.tmdb.org/t/p/original${poster_path}`} />
+    </div>
   )
 }
 
