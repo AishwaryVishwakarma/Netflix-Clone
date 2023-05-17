50d25d8904f0c42c10bb80b87644b76e51c0cbdf
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-05-18 01:07:36 +0530

    BANA DIYA SAB

--- src/components/commons/movieDetailModal/movieDetailModal.jsx
+++ src/components/commons/movieDetailModal/movieDetailModal.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from './styles.module.scss'
 import { createPortal } from 'react-dom'
 import { useEffect } from 'react'
+import { FaAudioDescription } from 'react-icons/fa'
+import { MdOutlineSubtitles, MdOutlineClose } from 'react-icons/md'
+import { BsFillPlayFill, BsHandThumbsUp } from 'react-icons/bs'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 const MovieDetailModal = ({ setIsModalOpen, movieDetail }) => {
   React.useEffect(() => {
@@ -39,19 +43,48 @@ const MovieDetailModal = ({ setIsModalOpen, movieDetail }) => {
         <img
           src={`https://image.tmdb.org/t/p/original${movieDetail.backdrop_path}`}
         ></img>
-        {/* {movieDetail.original_title} */}
+        <div className={styles.viewOptions}>
+          <div className={styles.playButton}>
+            <BsFillPlayFill className={`${styles.playIcon} ${styles.button}`} />{' '}
+            Play
+          </div>
+          <div className={styles.otherOptions}>
+            <AiOutlinePlus className={styles.button} />
+            <BsHandThumbsUp className={styles.button} />
+          </div>
+        </div>
+        <div
+          className={styles.closeButton}
+          onClick={() => {
+            setIsModalOpen(false)
+          }}
+        >
+          <MdOutlineClose className={styles.button} />
+        </div>
+
         <div className={styles.blurContainer}></div>
 
         <div className={styles.overviewContainer}>
           <div className={styles.firstColumn}>
             <div className={styles.statsContainer}>
-              <div className={styles.voteAverage}>
-                {movieDetail.vote_average *10}% Match
+              <div className={styles.matchPercentage}>
+                {Math.round(movieDetail.vote_average * 10)}% Match
+              </div>
+              <div className={styles.releaseDate}>
+                {movieDetail.releaseDate && movieDetail.release_date.substring(0, 4)}
               </div>
               <div>
-                {movieDetail.release_date}
+                {`${Math.round(movieDetail.runtime / 60)}h ${
+                  movieDetail.runtime % 60
+                } min`}
               </div>
-              
+              <div className={styles.icons}>
+                <FaAudioDescription />
+                <MdOutlineSubtitles />
+              </div>
+            </div>
+            <div className={styles.movieTitle}>
+              {movieDetail.original_title}
             </div>
             <div className={styles.storyDescription}>
               {movieDetail.overview}
