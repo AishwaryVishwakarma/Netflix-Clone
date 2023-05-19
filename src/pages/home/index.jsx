fb8200ad72b392ced0198bd0cea3f6ca0be08216
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-05-19 20:33:25 +0530

    added generic movie modal. debugg blurContainer

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react'
 import styles from './styles.module.scss'
 import Navbar from '../../components/Navbar/Navbar'
-import PopularMovies from '../../components/Home/Popular-Movies/PopularMovies'
 import Footer from '../../components/Footer/Footer'
 import GenericMovies from '../../components/Home/Generic-Movies/GenericMovies'
 import { BsFillPlayFill } from 'react-icons/bs'
@@ -15,23 +14,20 @@ const API_KEY = '9c3fd4dd152d57af68bd8d3ebd55fce0'
 const MOVIES_LIST = [
   {
     name: 'Popular Today',
-    type: 'Popular',
-    // api_endpoint: `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+    type: 'Popular'
   },
   {
     name: 'Harry Potter',
-    type: 'search',
-    // api_endpoint: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${this.name}`
+    type: 'search'
   },
+
   {
     name: 'Pirates of the caribbean',
-    type: 'search',
-    // api_endpoint: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=Pirates of the caribbean}`
+    type: 'search'
   },
   {
     name: 'Star Wars',
-    type: 'search',
-    // api_endpoint: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=Star Wars`
+    type: 'search'
   }
 ]
 
@@ -66,10 +62,7 @@ const HomePage = () => {
           </div>
         </div>
         {MOVIES_LIST.map((movie) => (
-          <GenericMovies
-            movieName={movie.name}
-            query={movie.type}
-          />
+          <GenericMovies movieName={movie.name} query={movie.type} />
         ))}
         <Footer />
       </div>
